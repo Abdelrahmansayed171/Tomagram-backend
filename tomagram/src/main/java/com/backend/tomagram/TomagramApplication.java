7b3f4bec12e461aacb826b00ef107ddb7e7ecedd
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-08-25 19:44:52 +0300

    spring boot project intializer

---
+++ tomagram/src/main/java/com/backend/tomagram/TomagramApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tomagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomagramApplication.class, args);
+    }
+
+}
---
+++ tomagram/src/test/java/com/backend/tomagram/TomagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tomagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
