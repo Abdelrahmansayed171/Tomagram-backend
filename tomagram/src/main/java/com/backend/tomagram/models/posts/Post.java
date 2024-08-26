22af7241eb81324fac19481a9afb5b4c13de6de3
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-08-26 23:07:30 +0300

    postEntityManagerFactory works well

---
+++ tomagram/src/main/java/com/backend/tomagram/models/posts/Post.java
@@ -0,0 +1,55 @@
+package com.backend.tomagram.models.posts;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Post {
+
+    @Id
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "birthdate", nullable = false)
+    private LocalDate birthdate;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "profile_picture")
+    private String profilePicture;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
