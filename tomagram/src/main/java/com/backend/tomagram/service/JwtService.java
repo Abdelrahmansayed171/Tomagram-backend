7a2c47574d84263d48edc4263922c79c9020d164
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-09-01 16:30:19 +0300

    tests passed successfully, Al-Hamdullelah

--- tomagram/src/main/java/com/backend/tomagram/service/JwtService.java
+++ tomagram/src/main/java/com/backend/tomagram/service/JwtService.java
@@ -1,10 +1,8 @@
 package com.backend.tomagram.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.io.IOException;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.security.PrivateKey;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +20,16 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "OTUxMGJhZDFlNWYwNWFkYTJiNmE5NjVjMWQwNjUzMTYxNTMxMTQwZTYxMjhmNTBmOTY3M2QyNDk5OGJlNzc1Ng==";
+    private static final String SECRET_KEY = generateKey();
+
+    private static String generateKey(){
+        String key = Encoders.BASE64.encode(Jwts.SIG.HS256.key().build().getEncoded());
+        System.out.println("***************************************************************************************");
+        System.out.println(key);
+        System.out.println(Arrays.toString(Decoders.BASE64.decode(key)));
+        System.out.println("***************************************************************************************");
+        return key;
+    }
     public String extractUsername(String jwt){
         return extractClaim(jwt, Claims::getSubject); // Subject should be the email or username
     }
@@ -67,7 +75,7 @@ public class JwtService {
         try {
             return Jwts
                     .parser() // Initialize the JWT parser
-                    .decryptWith(getSignInKey()) // Set the secret key for signature validation
+                    .verifyWith(getSignInKey()) // Set the secret key for signature validation
                     .build() // Build the JwtParser instance after setting the key
                     .parseSignedClaims(token) // Parse and validate the JWT, returning the parsed JWT object
                     .getPayload(); // Retrieve the claims (payload) from the parsed JWT
