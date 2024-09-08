b51c02b6ee905ea873395cb2b3c330fa0b2a60eb
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-09-08 11:39:37 +0300

    BioUpdate Request handled

--- tomagram/src/main/java/com/backend/tomagram/controller/UserController.java
+++ tomagram/src/main/java/com/backend/tomagram/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.backend.tomagram.controller;
 
-import com.backend.tomagram.repository.UserRepository;
-import com.backend.tomagram.service.JwtService;
+import com.backend.tomagram.dto.BioUpdateRequest;
 import com.backend.tomagram.service.UserService;
-import com.backend.tomagram.util.JwtUtil;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +18,9 @@ public class UserController {
 
     @PutMapping("/bio")
     public ResponseEntity<String> updateUserBio(@RequestHeader("Authorization") String authHeader,
-                                                @RequestBody String bio){
-        return userService.updateBio(authHeader,bio);
+                                                @RequestBody BioUpdateRequest bioRequest){
+
+        return userService.updateBio(authHeader,bioRequest.getBio());
     }
 
     @GetMapping("/bio/{username}")
