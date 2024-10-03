27306abc30dc2794d9319d131b72d0e04ac08e32
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-03 11:36:59 +0300

    install web socket dependency, web socket configuration added

---
+++ tomagram/src/main/java/com/backend/tomagram/config/application/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.backend.tomagram.config.application;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//      Enables a simple message broker that supports broadcasting messages to a specific topic.
+//      This allows multiple clients to subscribe to a topic and receive messages sent to that topic.
+        registry.enableSimpleBroker("/topic");
+
+//      Sets the prefix for message destinations.
+//      This prefix will be prepended to message destinations specified in the client's WebSocket messages.
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    // This method registers WebSocket endpoints that clients can connect to
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //  Adds a WebSocket endpoint at the path /chat.
+        //  .withSockJS() method enables SockJS fallback mechanisms for browsers that don't support WebSocket directly.
+        registry.addEndpoint("/chat").withSockJS();
+    }
+}
