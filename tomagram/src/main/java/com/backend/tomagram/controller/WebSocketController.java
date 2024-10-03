026c948d43534dddfbec787437ac33ecbdfab551
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-03 11:56:45 +0300

    message mapping done in WebSocketController

---
+++ tomagram/src/main/java/com/backend/tomagram/controller/WebSocketController.java
@@ -0,0 +1,21 @@
+package com.backend.tomagram.controller;
+
+import com.backend.tomagram.models.Message;
+import com.backend.tomagram.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final ChatService chatService;
+
+    @MessageMapping("/chat/{conversationId}")
+    @SendTo("/topic/{conversationId}")
+    public Message sendMessage(@DestinationVariable String conversationId, Message message) throws Exception{
+        return chatService.sendMessage(conversationId, message.getSender(), message.getContent());
+    }
+}
