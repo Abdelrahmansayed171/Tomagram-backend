beadee70927922960fe150cec3865d6f8782a258
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-07 18:38:15 +0300

    added elastic search documents for posts, and comments

---
+++ tomagram/src/main/java/com/backend/tomagram/models/CommentDocument.java
@@ -0,0 +1,24 @@
+package com.backend.tomagram.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "comments")
+public class CommentDocument {
+    @Id
+    private String id;
+    private String content;
+    private String postId;
+    private String userId;
+    private LocalDateTime timestamp;
+}
