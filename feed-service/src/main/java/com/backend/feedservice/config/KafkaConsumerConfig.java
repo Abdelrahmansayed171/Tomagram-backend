3cd1f516bd778e57380cc73f7f371d295672b2c5
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-11 21:49:18 +0300

    Kafka consumers setup at feed-service

--- feed-service/src/main/java/com/backend/feedservice/config/KafkaConsumerConfig.java
+++ feed-service/src/main/java/com/backend/feedservice/config/KafkaConsumerConfig.java
@@ -1,9 +1,7 @@
-package com.backend.tomagram.config.application;
+package com.backend.feedservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +10,7 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.xml.sax.ErrorHandler;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,7 +41,7 @@ public class KafkaConsumerConfig {
     @Bean
     public KafkaListenerContainerFactory<
             ConcurrentMessageListenerContainer<String, Object>> factory(
-                    ConsumerFactory<String, Object> consumerFactory){
+            ConsumerFactory<String, Object> consumerFactory){
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
--- feed-service/src/main/java/com/backend/feedservice/listener/FeedEventListener.java
+++ feed-service/src/main/java/com/backend/feedservice/listener/FeedEventListener.java
@@ -1,31 +1,39 @@
-package com.backend.tomagram.listener;
+package com.backend.feedservice.listener;
 
-import com.backend.tomagram.dto.PostRequest;
-import com.backend.tomagram.dto.SeenPostRequest;
-import com.backend.tomagram.dto.UploadRequest;
+import com.backend.feedservice.dto.PostRequest;
+import com.backend.feedservice.dto.SeenPostRequest;
+import com.backend.feedservice.dto.UploadRequest;
+import com.backend.feedservice.service.FeedService;
+import com.backend.feedservice.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FeedEventListener {
+    private final FeedService feedService;
+    private final PostService postService;
+
     @KafkaListener(topics = "tomagram-feed", groupId = "groupId")
     public void handleAddFeed(UploadRequest request) {
         // Process the UploadRequest
         System.out.println("Received UploadRequest: " + request);
-        // Add your business logic here
+        feedService.fanout(request);
     }
 
     @KafkaListener(topics = "tomagram-update", groupId = "groupId")
     public void handleUpdatePost(PostRequest request) {
         // Process the PostRequest
         System.out.println("Received PostRequest: " + request);
-        // Add your business logic here
+        postService.storePost(request);
     }
+
 
     @KafkaListener(topics = "tomagram-seen", groupId = "groupId")
     public void handleSeenPost(SeenPostRequest request) {
         // Process the SeenPostRequest
         System.out.println("Received SeenPostRequest: " + request);
-        // Add your business logic here
+        feedService.markPostsAsSeen(request);
     }
 }
