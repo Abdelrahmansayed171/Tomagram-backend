e6692ac9e11a67d535ead18aa8aeac582e77d84f
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-11 19:45:20 +0300

    added feed service to publish requests into Kafka topics

---
+++ tomagram/src/main/java/com/backend/tomagram/service/FeedService.java
@@ -0,0 +1,29 @@
+package com.backend.tomagram.service;
+
+import com.backend.tomagram.dto.PostRequest;
+import com.backend.tomagram.dto.SeenPostRequest;
+import com.backend.tomagram.dto.UploadRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void addFeed(UploadRequest request) {
+        // Send the UploadRequest object to Kafka
+        kafkaTemplate.send("tomagram-feed", request);
+    }
+
+    public void updatePost(PostRequest request) {
+        // Send the PostRequest object to Kafka
+        kafkaTemplate.send("tomagram-update", request);
+    }
+
+    public void addPostToSeen(SeenPostRequest request) {
+        // Send the SeenPostRequest object to Kafka
+        kafkaTemplate.send("tomagram-seen", request);
+    }
+}
